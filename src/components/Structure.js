641b4cb1a5c84502dd57cf5166f5b3eed458589c
zprochazkova <zuzana.prochazkova@mibcon.cz>
2023-06-19 17:04:10 +0200

    styled dropdown

--- src/components/Structure.js
+++ src/components/Structure.js
@@ -66,7 +66,7 @@ const Structure = () => {
             >
                 <h1 style={{ marginBottom: "30px", marginTop: "30px", }}>MINE HUNTER</h1>
             </div>
-            <div style={{ display: "flex", justifyContent: "center", width: "650px", margin: "0 auto" }}>
+            <div style={{ display: "flex", justifyContent: "center", width: "850px", margin: "0 auto" }}>
                 <TextField
                     fullWidth
                     id="fullWidth"
@@ -78,7 +78,7 @@ const Structure = () => {
                         padding: "10px 25px",
                         marginBottom: "18px",
                         borderRadius: "6px",
-                        width: "600px",
+                        width: "800px",
                         boxShadow: "0 0 10px 10px #0000001F",
                     }}
                     type="text"
@@ -86,11 +86,24 @@ const Structure = () => {
                     variant="standard"
                     onChange={handleInputChange}
                 />
-                <FormControl>
-                    <InputLabel id="category-select-label">Category</InputLabel>
+                <FormControl
+                             style={{
+                    alignItems: "center",
+                    border: "none",
+                    display: "flex",
+                    backgroundColor: "white",
+                    margin: "0 10px 18px 10px",
+                    borderRadius: "6px",
+                    width: "350px",
+                    boxShadow: "0 0 10px 10px #0000001F",
+                }}>
+                    <InputLabel disableAnimation={"false"} id="category-select-label">Category</InputLabel>
                     <Select
-                        labelId="category-select-label"
                         id="category-select"
+                        size="medium"
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        fullWidth
                         value={selectedCategory}
                         onChange={handleCategoryChange}
                     >
@@ -105,12 +118,11 @@ const Structure = () => {
                     className="hovered-button"
                     style={{
                         display: "flex",
+                        height: "56px",
                         borderRadius: "6px",
-                        height: "52px",
                         border: "none",
                         backgroundColor: "white",
                         padding: "10px",
-                        margin: "0 0 0 10px",
                         alignItems: "center",
                         justifyContent: "flex-start",
                         fontWeight: "bold",
@@ -127,13 +139,15 @@ const Structure = () => {
                 <div
                     style={{
                         display: "flex",
-                        textAlign: "left",
-                        width: "50%",
+                        width: "800px",
                         backgroundColor: "white",
                         padding: "25px",
                         marginBottom: "20px",
                         borderRadius: "8px",
                         alignItems: "center",
+                        justifyContent: "center",
+                        margin: "0 auto",
+                        boxShadow: "0 0 10px 10px #0000001F",
                     }}
                 >
                     <p style={{ width: "95%", lineHeight: "1.5", fontSize: "14px" }}>
@@ -144,13 +158,13 @@ const Structure = () => {
 
             {responses.map((response, index) => (
                 <div
-                    style={{ display: "flex", flexDirection: "row", textAlign: "left", width: "600px", backgroundColor: "white", padding: "25px", margin: "15px auto", borderRadius: "8px", }}
+                    style={{ boxShadow: "rgba(0, 0, 0, 0.12) 0px 0px 10px 10px", display: "flex", flexDirection: "row", textAlign: "left", width: "800px", backgroundColor: "white", padding: "25px", margin: "15px auto", borderRadius: "8px", }}
                     key={index}
                 >
                     <img
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Robot_icon.svg/96px-Robot_icon.svg.png?20160602132836"
                         alt="Loading"
-                        style={{ marginRight: "8px", width: "44px", height: "44px" }}
+                        style={{ marginRight: "8px", width: "32px", height: "32px", marginTop: "18px" }}
                     />
                     <p
                         style={{
@@ -158,19 +172,19 @@ const Structure = () => {
                             width: "95%",
                             lineHeight: "1.5",
                             fontSize: "14px",
+                            wordBreak: "break-word",
                         }}
                     >
-                        <b>Question:</b> {response.Prompt}
+                        <strong style={{ marginBottom: "16px" }}>Question:</strong> {response.Prompt}
                         {/*{JSON.stringify(response)}*/}
-                        <br/>
-                        <br/>
-                        <b>Answer:</b> {response.Answer}
-                        <br/>
-                        <br/>
-                        <b>Sources:</b> {response.Sources.map((source) =>(<div>
-                            <b>{source[0]}</b> - {source[1]}
-                            <br/>
-                            <br/>
+
+                        <strong style={{ marginBottom: "16px" }}>Answer:</strong> {response.Answer}
+
+                        <strong style={{ marginBottom: "16px" }}>Sources:</strong> {response.Sources.map((source) =>(
+
+                            <div style={{ marginBottom: "16px" }}>
+                            <strong>{source[0]}</strong> - {source[1]}
+
                         </div>
 
 
